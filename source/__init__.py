c60f96518418f1a3b333fee99aa3fc1edbeea666
srmnitc <sarathmenon@mailbox.org>
2020-03-31 20:13:35 +0200

    initial commit to convert to package

--- source/__init__.py
+++ source/__init__.py
@@ -5,3 +5,6 @@
 __author__ = """Alexander Hartmaier"""
 __email__ = 'alexander.hartmaier@rub.de'
 __version__ = '1.1'
+
+import pyLabFEA.pyLabFEM as pyLabFEM
+import pyLabFEA.pyLabMaterial as pyLabMaterial 
