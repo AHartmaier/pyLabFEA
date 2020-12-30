Some bug fixes and improvements in equivalent stress calculation

pylabfea/basic.py
@@ -6,16 +6,21 @@ for efficient operations with these quantities.
 
 uses NumPy
 
-Version: 3.1 (2020-10-25)
+Version: 3.2 (2020-12-30)
 Author: Alexander Hartmaier, ICAMS/Ruhr-University Bochum, April 2020
 Email: alexander.hartmaier@rub.de
 distributed under GNU General Public License (GPLv3)'''
 
 __all__ = ['Strain', 'Stress', 'a_vec', 'b_vec', 'eps_eq', 'polar_ang', 'ptol', 
-           's_cyl', 'seq_J2', 'sp_cart']
+           's_cyl', 'seq_J2', 'sp_cart', 'svoigt', 'sprinc']
 
 import numpy as np
 import sys
+'''try:
+    import quaternion
+    av_quater = True
+except:
+    av_quater = False'''
 
 #===================================
 #define global methods and variables
@@ -27,7 +32,7 @@ b_vec = np.array([0.,  0.5, -0.5])*np.sqrt(2)
 ptol = 3.e-3
 '''Tolerance: Plastic yielding if yield function > ptol'''
 def seq_J2(sig):
-    '''Calculate J2 equivalent stress from principal stresses
+    '''Calculate J2 equivalent stress from any stress tensor
     
     Parameters
     ----------
@@ -43,10 +48,10 @@ def seq_J2(sig):
     N = len(sig)
     sh = np.shape(sig)
     if sh==(3,):
-        N = 1 # sprinc is single principle stress vector
+        N = 1 # sig is single principle stress vector
         sprinc=np.array([sig])
     elif sh==(6,):
-        N = 1 # sprinc is single Voigt stress
+        N = 1 # sig is single Voigt stress
         sprinc=np.array([Stress(sig).p])
     elif sh==(N,6):
         sprinc=np.zeros((N,3))
@@ -67,7 +72,7 @@ def seq_J2(sig):
     return seq
 
 def polar_ang(sig):
-    '''Transform principal stresses into polar angle on deviatoric plane spanned by a_vec and b_vec
+    '''Transform stresses into polar angle on deviatoric plane spanned by a_vec and b_vec
     
     Parameters
     ----------
@@ -95,16 +100,93 @@ def polar_ang(sig):
         sprinc=np.array(sig)
     else:
         print('*** polar_angle: N, sh', N, sh, sys._getframe().f_back.f_code.co_name)
-        raise FormatError('Error: Unknown format of stress in polar_angle')
+        raise TyeError('Error: Unknown format of stress in polar_angle')
     hyd = np.sum(sprinc,axis=1)/3.  # hydrostatic component
-    dev = sprinc - np.array([hyd, hyd, hyd]).transpose() # deviatoric princ. stress
-    vn  = np.linalg.norm(dev,axis=1)  #norms of stress vectors
-    dsa = np.dot(dev,a_vec)
-    dsb = np.dot(dev,b_vec)
+    dev = sprinc - hyd[:,None] # deviatoric princ. stress
+    vn  = np.linalg.norm(dev,axis=1)  #norms of princ. stress vectors
+    ind = np.nonzero(vn<1.e-4)[0]
+    vn[ind] = 1.
+    dsa = np.dot(dev/vn[:,None],a_vec)
+    dsb = np.dot(dev/vn[:,None],b_vec)
     theta = np.angle(dsa + 1j*dsb)
     if sh==(3,) or sh==(6,):
         theta=theta[0]
     return theta
+
+def sprinc(sig):
+    '''Convert Voigt stress tensors into principal stresses and eigenvectors.
+    
+    Parameters
+    ----------
+    sig : (6,), (N,6), (3,3), or (N,3,3) array
+        Voigt stress tensor (dim=6) or Cartesian stress tensor (dim=3x3)
+        
+    Returns
+    -------
+    spa : (3,) or (N,3) array
+        Principal stresses
+    eva : (3,3) or (N,3,3) array
+        Eigenvectors/rotation matrices of stress tensor
+    '''
+    N = len(sig)
+    sh = np.shape(sig)
+    if sh==(3,3):
+        N = 1 # sig is Cartesian single stress tensor
+        st=np.array([sig])
+    elif sh==(N,3,3):
+        st=np.array(sig) # sig is array of Cart. stress tensors
+    elif sh==(6,):
+        N = 1 # sig is single Voigt stress
+        st = np.zeros((3,3))
+        st[0,0]=sig[0]
+        st[1,1]=sig[1]
+        st[2,2]=sig[2]
+        st[2,1]=st[1,2]=sig[3]
+        st[2,0]=st[0,2]=sig[4]
+        st[1,0]=st[0,1]=sig[5]
+    elif sh==(N,6):
+        st=np.zeros((N,3,3))
+        for i in range(N):
+            st[i,0,0]=sig[i,0]
+            st[i,1,1]=sig[i,1]
+            st[i,2,2]=sig[i,2]
+            st[i,2,1]=st[i,1,2]=sig[i,3]
+            st[i,2,0]=st[i,0,2]=sig[i,4]
+            st[i,1,0]=st[i,0,1]=sig[i,5]
+    else:
+        print('*** sprinc: N, sh', N, sh, sys._getframe().f_back.f_code.co_name)
+        raise TypeError('Error: Unknown format of stress in sprinc')
+        
+    #calculate principal stresses and eigen vectors
+    spa = np.zeros((N,3))
+    eva = np.zeros((N,3,3))
+    for n in range(N):
+        sp, ev = np.linalg.eig(st[n]) # solve eigenvalue problem 
+        #arrange principal stress components according to major force axes
+        iev = np.argmax(np.abs(ev),axis=1)
+        j = np.zeros(3, dtype=int)
+        i0 = [i for i,x in enumerate(iev) if x == 0] # positions of indices 0
+        i1 = [i for i,x in enumerate(iev) if x == 1]
+        i2 = [i for i,x in enumerate(iev) if x == 2]
+        k0 = len(i0)
+        for i in range(k0):
+            j[i] = i0[i]
+        for i in range(len(i1)):
+            j[k0+i] = i1[i]
+        k0 += len(i1)
+        for i in range(len(i2)):
+            j[k0+i] = i2[i]
+        ev  = np.array([ev[j[0],:], ev[j[1],:], ev[j[2],:]])
+        sp = np.array([sp[j[0]], sp[j[1]], sp[j[2]]])
+        #ensure positive determinant
+        if np.linalg.det(ev)<0:
+            ev *= -1
+        spa[n,:] = sp
+        eva[n,:,:] = ev
+    if sh==(3,3) or sh==(6,):
+        spa = spa[0]
+        eva = eva[0,:,:]
+    return spa, eva
 
 def sp_cart(scyl):
     '''Convert cylindrical stress into 3D Cartesian vector of deviatoric principle stresses 
@@ -134,13 +216,41 @@ def sp_cart(scyl):
         sprinc=sprinc[0]
     return sprinc
 
-def s_cyl(sprinc, mat=None):
+def svoigt(scyl, evec):
+    '''Convert cylindrical stress and eigenvectors into Voigt stress tensor
+    
+    Parameters
+    ----------
+    scyl : (3,) or (N,3) array 
+        Cylindrical stress in form (seq, theta, p)
+    evec : (3,3) or (N,3,3) array 
+        Eigenvectors of stress tensor
+         
+    Returns
+    -------
+    svoigt : (6,) or (N,6) array
+        Voigt stress tensor
+    '''
+    #sh = np.shape(scyl) 
+    #if sh==(3,):
+    #    scyl = np.array([scyl])
+    sp = sp_cart(scyl)
+    if np.linalg.det(evec)<0:
+        evec *= -1  # enforce right-handed system of eigenvectors
+    st = np.diag(sp)  # diag. matrix of princ. stresses
+    hh = evec@st@evec.T  # rotate back into original stress frame
+    svoigt = np.array([hh[0,0], hh[1,1], hh[2,2], hh[1,2], hh[0,2], hh[0,1]]) 
+    #if sh==(3,):
+    #    svoigt=svoigt[0]
+    return svoigt
+
+def s_cyl(sig, mat=None):
     '''convert principal stress into cylindrical stress vector 
     
     Parameters
     ----------
-    sprinc : (3,) or (N,3) array 
-         principal stresses
+    sig : (3,), (6,), (N,3) or (N,6) array 
+         stress to be converted, if (3,) or (N,3) principal stress is assumed
     mat : object of class ``Material``
         Material for Hill-type principal stress (optional) 
          
@@ -149,19 +259,32 @@ def s_cyl(sprinc, mat=None):
     sc : (3,) or (N,3) array
         stress in cylindrical coordinates (seq, theta, p)
     '''
-    sh = np.shape(sprinc) 
-    N = len(sprinc)
+    sh = np.shape(sig) 
+    N = len(sig)
     if sh==(3,):
-        sprinc = np.array([sprinc])
+        N = 1 # sig is single principle stress vector
+        sp  = np.array([sig])
+        sig = np.array([[sig[0], sig[1], sig[2], 0, 0,0]])
+    elif sh==(N,3):
+        sp  = np.array(sig)
+        sig = np.append(sig,np.zeros((N,3)), axis=1)
+    elif sh==(6,):
         N = 1
+        sp  = np.array([Stress(sig).princ])
+        sig = np.array([sig])
+    elif sh==(N,6):
+        sp = sprinc(sig)[0]
+    else:
+        print('*** s_cyl (N,sh): ',N,sh,sys._getframe().f_back.f_code.co_name)
+        raise TypeError('Error in s_cyl: Format not supported')
     sc = np.zeros((N,3))
     if mat is None:
-        sc[:,0] = seq_J2(sprinc)
+        sc[:,0] = seq_J2(sp)
     else:
-        sc[:,0] = mat.calc_seq(sprinc)
-    sc[:,1] = polar_ang(sprinc)
-    sc[:,2] = np.sum(sprinc, axis=1)/3.
-    if sh==(3,):
+        sc[:,0] = mat.calc_seq(sig)
+    sc[:,1] = polar_ang(sp)
+    sc[:,2] = np.sum(sp, axis=1)/3.
+    if sh==(3,) or sh==(6,):
         sc=sc[0]
     return sc
 
@@ -193,7 +316,7 @@ def eps_eq(eps):
         eeq = np.sqrt(2.*np.sum(eps[:,0:3]*eps[:,0:3],axis=1)/3.)
     else:
         print('*** eps_eq (N,sh): ',N,sh,sys._getframe().f_back.f_code.co_name)
-        raise FormatError('Error in eps_eq: Format not supported')
+        raise ValueError('Error in eps_eq: Format not supported')
 
     if sh==(6,) or sh==(3,):
         eeq = eeq[0]
@@ -234,10 +357,10 @@ class Stress(object):
         self.tens[2,0]=self.tens[0,2]=sv[4]
         self.tens[1,0]=self.tens[0,1]=sv[5]
         #calcualte principal stresses and eigen vectors
-        self.princ, self.evec = np.linalg.eig(self.tens)
-        self.p=self.princ
-        self.h=self.hydrostatic = np.sum(self.p)/3.
-        self.d=self.dev = sv - np.array([self.h, self.h, self.h, 0., 0., 0.])
+        self.princ, self.evec = sprinc(self.tens)
+        self.p = self.princ
+        self.h = self.hydrostatic = np.sum(self.p)/3.
+        self.d = self.dev = sv - np.array([self.h, self.h, self.h, 0., 0., 0.])
             
     def seq(self, mat):
         '''calculate Hill-type equivalent stress, invokes corresponding method of class ``Material``
@@ -252,7 +375,10 @@ class Stress(object):
         seq : float
             equivalent stress of Hill-type
         '''
-        seq = mat.calc_seq(self.p)
+        if mat.hill_6p:
+            seq = mat.calc_seq(self.v)
+        else:
+            seq = mat.calc_seq(self.p)
         return seq
     
     def theta(self):
@@ -276,6 +402,17 @@ class Stress(object):
         '''
         sJ2 = seq_J2(self.p)
         return sJ2
+    
+    def cyl(self):
+        '''Calculate cylindrical stress tensor
+        
+        Returns
+        -------
+        cyl : (3,) array
+            stress in cylindrical form: (J2 eqiv. stress, polar angle, hydrostatic)
+        '''
+        cyl = np.array([seq_J2(self.p), polar_ang(self.p), self.h])
+        return cyl
     
     def lode_ang(self, X):
         '''Calculate Lode angle:  

pylabfea/model.py
@@ -5,7 +5,7 @@ module pylabfea.material
 
 uses NumPy, SciPy, MatPlotLib
 
-Version: 3.1 (2020-10-25)
+Version: 3.2 (2020-12-30)
 Author: Alexander Hartmaier, ICAMS/Ruhr-University Bochum, April 2020
 Email: alexander.hartmaier@rub.de
 distributed under GNU General Public License (GPLv3)'''
@@ -122,6 +122,7 @@ class Model(object):
         self.epgl = np.zeros((1,6)) # list for time evolution of global plastic strain
         self.u = None
         self.f = None
+        self.Nnode = None
         #SRM : named module glob exists
         self.glob = {
             'ebc1'   : None,  # global x-strain from BC
@@ -559,7 +560,7 @@ class Model(object):
             NY = 1
             warnings.warn('Warning: NY=1 for 1-d model')
         if self.u is not None:
-            #print('Warning: solution of previous solution steps is deleted')
+            warnings.warn('Warning: Solution of previous steps is deleted')
             self.u = None
             self.f = None
         self.NnodeX = self.shapefact*NX + 1    # number of nodes along x axis
@@ -690,7 +691,7 @@ class Model(object):
         Model.u     : (Model.Ndof,) array
             Nodal displacements
         Model.f     : (Model.Ndof,) array
-            Nodel forces
+            Nodal forces
         Model.sgl   : (N,6) array
             Global stress as as Voigt tensor for each incremental load step (homogenized element solution)
         Model.egl   : (N,6) array
@@ -704,6 +705,10 @@ class Model(object):
         Element.epl : (6,) array
             Element solution for plastic strain tensor
         '''
+        #test if meshing has been performed
+        if self.Nnode is None:
+            raise AttributeError('Attributes for mesh not set, but required by solver.')
+        
         #calculate reduced stiffness matrix according to BC
         def Kred(K, ind):
             Kred = np.zeros((len(ind), len(ind)))