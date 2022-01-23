475a327ca88a25352c9a7d29c4c1fe7dbcf80412
Alexander Hartmaier <alexander.hartmaier@rub.de>
2022-01-23 20:17:15 +0100

    Updated to version 4.1

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/AHartmaier/pyLabFEA',
-    version='4.0',
+    version='4.1',
     zip_safe=False,
 )
