233876e421bd9336eafab8ee4093737f01485c0e
AHartmaier <alexander.hartmaier@rub.de>
2025-01-05 10:23:47 +0100

    Version 4.3.4 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/AHartmaier/pyLabFEA',
-    version='4.3.3',
+    version='4.3.4',
     zip_safe=False,
 )
