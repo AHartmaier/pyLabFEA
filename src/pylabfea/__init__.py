eb836e9f3415862dacbd155b671d46ebc3288dba
Jan Schmidt <jan.schmidt-p2d@rub.de>
2025-07-18 10:35:07 +0200

    MergePrep: import 2 new functions.

--- src/pylabfea/__init__.py
+++ src/pylabfea/__init__.py
@@ -5,13 +5,13 @@
 from pylabfea.basic import Strain, Stress, a_vec, b_vec, yf_tolerance, \
     eps_eq, sig_polar_ang, yf_tolerance, sig_princ2cyl, \
     sig_eq_j2, sig_cyl2princ, sig_cyl2voigt, sig_princ, \
-    pickle2mat, sig_dev, \
+    pickle2mat, sig_dev, sig_spherical_to_cartesian, \
     seq_J2, sprinc, sp_cart, svoigt, s_cyl, sdev  # legacy
 
 from pylabfea.model import Model
 from pylabfea.material import Material
 from pylabfea.data import Data, find_transition_index, get_elastic_coefficients
-from pylabfea.training import load_cases, training_score
+from pylabfea.training import load_cases, training_score, create_test_sig
 from importlib.metadata import version
 
 __author__ = """Alexander Hartmaier, Ronak Shoghi"""
